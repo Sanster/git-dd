240d508c741f08d8d9e7f13ee25f7ca4cf4a0d43
Sanster <cwq1913@163.com>
2017-07-16 21:35:18 +0800

    show verbose branch info

--- lib/git-dd.rb
+++ lib/git-dd.rb
@@ -5,18 +5,25 @@ require 'rainbow/ext/string'
 
 class GitDD
   def run
-    branches = `git branch`
+    branch_names = `git branch`
     return if $?.exitstatus != 0
+
+    branch_names = branch_names.split("\n")
+
+    branches_with_more_info = `git branch -vv`
+    branches_with_more_info = branches_with_more_info.split("\n")
+
+    return if branch_names.size != branches_with_more_info.size
+
+    branches = {}
+    branch_names.each_with_index { |b, i| branches[b] = branches_with_more_info[i] }
 
     if branches.size == 1
       puts "You only have one branch."
       return
     end
 
-    branches = branches.split("\n").select do |b|
-      b != current_branch_with_mark && !b.include?("* (HEAD detached at")
-    end
-
+    branches = branches.select { |k, v| k != current_branch_with_mark && !k.include?("* (HEAD detached at") }
 
     puts "Current branch is: #{current_branch.color(:green)}"
 
@@ -28,12 +35,18 @@ class GitDD
       end
     end
 
-    branches_to_delete = prompt.multi_select("Choose branches to delete:", branches, per_page: 20, help: '')
+    branches_to_delete = prompt.multi_select("Choose branches to delete:", per_page: 20, help: '',echo: false) do |menu|
+      branches.each { |k, v| menu.choice v, k}
+    end
+
+    branches_to_delete.each { |b| puts b.color(:red) }
 
     ensure_delete = !prompt.no?('Are you sure?')
 
     if ensure_delete
-      branches_to_delete.each { |b| puts `git branch -D #{b}`}
+      branches_to_delete.each do |b|
+        puts `git branch -D #{b}`.chomp.color(:yellow)
+      end
     end
   end
 
